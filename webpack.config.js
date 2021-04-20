0d21d6d6a4298f58651927499e02c8092f3832ee
rafalWili <rafal.wili.uk@gmail.com>
2021-04-20 21:15:29 +0200

    basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+// Webpack uses this to work with directories
+const path = require('path');
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin its work
+  entry: './assets/src/index.js',
+
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'bundle.js'
+  },
+
+  // minifying  development mode
+  mode: 'development'
+};

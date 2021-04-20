8ba41818346bd85d5778fd98292da526ee3690b0
rafalWili <rafal.wili.uk@gmail.com>
2021-04-20 23:45:42 +0200

    additional configuration for the webpack scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,65 @@
-// Webpack uses this to work with directories
+
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // This is the main configuration object.
 // Here, you write different options and tell Webpack what to do
 module.exports = {
 
   // Path to your entry point. From this file Webpack will begin its work
-  entry: './assets/js/index.js',
+  entry: ['./assets/js/index.js','./assets/sass/index.scss' ],
 
   // Webpack will bundle all JavaScript into this file
   output: {
-    path: path.resolve(__dirname, 'assets/dist'),
+    path: path.resolve(__dirname, 'assets'),
     publicPath: '',
-    filename: 'bundle.js'
+    filename: 'dist/bundle.js'
   },
+  watchOptions: {
+    aggregateTimeout: 200,
+    poll: 1000,
+  },
+  watch:true,
+  
+  module: {
+  rules: [
+    {
+      test: /\.js$/,
+      /* ... */
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: './css/style.css',
+          }
+        },
+        {
+          loader: 'extract-loader'
+        },
+        {
+          loader: 'css-loader?-url'
+        },
+        {
+          loader: 'postcss-loader'
+        },
+        {
+          loader: 'sass-loader'
+        }
+      ]
+    }
+]
+  },
+  plugins: [
+    // Define the filename pattern for CSS.
+    new MiniCssExtractPlugin({
+      filename: './assets/style.css',
+    })
+  ],
 
   // minifying  development mode
   mode: 'development'

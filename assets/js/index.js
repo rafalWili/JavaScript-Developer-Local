318911757e75a2b7d877d4c594680cc52a5725bc
rafalWili <rafal.wili.uk@gmail.com>
2021-04-21 07:28:23 +0200

    jquery validate form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,38 @@
-console.log('lets start')
+console.log('lets start');
+
+import $ from 'jquery';
+import jqueryValidation from 'jquery-validation';
+
+
+$("#registration_form").validate({
+    rules: {
+        user_name:{
+            required: true,
+            minlength: 2            
+        },
+        user_email: {
+          required: true,
+          email: true         
+        },
+        pass1 : {
+            minlength : 5
+        },
+        pass2 : {
+            minlength : 5,
+            equalTo : "#pass1"
+        }
+    },
+        messages: {
+            user_name : {
+            required: "Required input!",
+            minlength: $.validator.format("Please, at least {0} characters are necessary")
+            },
+            user_email : {
+                required: "Required input!",
+
+            }
+            
+          }
+        
+    
+  });
--- footer.php
+++ footer.php
@@ -3,27 +3,40 @@
     <div class="row">  
         <div class="col">  
 
-        <form action="" method="POST" >
+        <form id="registration_form" action="" method="POST" >
         <div class="card w-100   p-3">
             <h4 class="w-100 pb-3">Registration Form</h4>
             <div class="d-flex flex-row"> 
+              <div class="col-12 col-md-6">
                 <div class="form-group">
-                  <label for="user_name">Name</label>
-                  <input type="text" class="form-control" id="user_name" aria-describedby="user_name">
+                  <label for="user_name">Name*</label>
+                  <input type="text" name="user_name" class="form-control" id="user_name" aria-describedby="user_name" required>
                 </div>
-                <div class="form-group mx-5">
-                  <label for="InputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
+                <div class="form-group ">
+                  <label for="InputEmail1">Email address*</label>
+                  <input type="email" class="form-control" id="user_email" name="user_email" aria-describedby="unique-id-here" >
+                  <!-- <span class="error" id="unique-id-here">This field is required</span> -->
                   
                 </div>
+              </div>
+              <div class="col-12 col-md-6">
                 <div class="form-group">
-                  <label for="InputPassword1">Password</label>
-                  <input type="password" class="form-control" id="InputPassword1">
+                  <label for="InputPassword1">Password*</label>
+                  <input type="password" class="form-control" name="pass1" id="pass1" required>
                 </div>
-                <div class="d-flex align-items-center">
-                  <button id="form_submit" type="submit" class="btn btn-primary mt-3 ml-5 ">Submit</button>
+                <div class="form-group ">
+                  <label for="InputPassword2">Password*</label>
+                  <input type="password" class="form-control" name="pass2" id="pass2" required>
                 </div>
               </div>
+             
+              </div>
+
+              <div class="col-12 col-md-12">
+                <div class="float-right mt-3">
+                  <button id="form_submit" type="submit" class="btn btn-primary mt-n2 ml-5 ">Submit</button>
+                </div>
+             </div>
             </div>
         </form>
         </div> 
